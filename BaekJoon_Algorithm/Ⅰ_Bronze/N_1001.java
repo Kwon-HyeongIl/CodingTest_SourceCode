22d303607323f7673903881664ad61a3a54077f6
KHI <llioopv@naver.com>
2023-01-17 19:37:10 +0900

    A-B

---
+++ BaekJoon_Algorithm/Ⅰ_Bronze/N_1001.java
@@ -0,0 +1,19 @@
+package baekjoon_Algorithm;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class N_1001 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int A = Integer.valueOf(st.nextToken());
+		int B = Integer.valueOf(st.nextToken());
+		
+		System.out.print(A-B);
+		br.close();
+	}
+}
