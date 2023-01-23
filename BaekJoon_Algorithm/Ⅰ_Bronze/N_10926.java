612b795e993b93592f44c05323b175e5ea3bd6fc
Kwon HyeongIl <119515797+Kwon-HyeongIl@users.noreply.github.com>
2023-01-24 02:26:08 +0900

    ??!

---
+++ BaekJoon_Algorithm/Ⅰ_Bronze/N_10926.java
@@ -0,0 +1,15 @@
+package baekjoon_Algorithm;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class N_10926 {
+
+    public static void main(String[] args) throws IOException {
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    	StringBuilder sb = new StringBuilder();
+    	sb.append(br.readLine()).append("??!");
+    	System.out.println(sb);
+    }
+}
