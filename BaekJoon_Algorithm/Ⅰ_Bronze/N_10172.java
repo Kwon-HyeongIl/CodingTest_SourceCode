b7ee1624e0ea988f148ed2409a2ffb181cf1369f
Kwon HyeongIl <119515797+Kwon-HyeongIl@users.noreply.github.com>
2023-01-24 02:20:18 +0900

    개

---
+++ BaekJoon_Algorithm/Ⅰ_Bronze/N_10172.java
@@ -0,0 +1,15 @@
+package baekjoon_Algorithm;
+
+public class N_10172 {
+
+    public static void main(String[] args) {
+    StringBuilder sb = new StringBuilder();
+    sb.append("|\\_/|\n");
+    sb.append("|q p|   /}\n");
+    sb.append("( 0 )\"\"\"\\\n");
+    sb.append("|\"^\"`    |\n");
+    sb.append("||_/=\\\\__|");
+    
+    System.out.println(sb);
+    }
+}
